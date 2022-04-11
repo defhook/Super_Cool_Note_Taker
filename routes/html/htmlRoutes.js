a7a2818f4de5074302d1f28c4bf4af1158e4f2f2
defhook <defhook152@gmail.com>
2022-04-10 22:09:16 -0700

    updated heroku

--- routes/html/htmlRoutes.js
+++ routes/html/htmlRoutes.js
@@ -2,8 +2,6 @@ const fs = require('fs');
 const path = require('path');
 const router = require('express').Router();
 
-// HTML routes
-
 // get notes page
 router.get('/notes', (request, response) => {
     response.sendFile(path.join(__dirname,'../../public/notes.html'))
